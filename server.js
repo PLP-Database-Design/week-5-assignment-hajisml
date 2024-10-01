10dcbfb8b870758142d7f88e4f7c705b1efe5780
Haji Ibrahim <halimke@outlook.com>
2024-10-01 17:48:04 +0300

    requirements and dependancies

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
-
-// Question 1 goes here
-
-
-// Question 2 goes here
-
-
-// Question 3 goes here
-
-
-// Question 4 goes here
-
-
+app.use(express.json());
+app.use(cors());
 
 // listen to the server
-const PORT = 3000
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`server is runnig on http://localhost:${PORT}`)
-})
+  console.log(`server is running on http://localhost:${PORT}`);
+});
