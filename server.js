9827bf03fc4f5370ffbfea17b0353ab42f3238fb
Haji Ibrahim <halimke@outlook.com>
2024-10-01 14:26:29 +0300

    added basic server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+
+// Question 1 goes here
+
+
+// Question 2 goes here
+
+
+// Question 3 goes here
+
+
+// Question 4 goes here
+
+
+
+// listen to the server
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`server is runnig on http://localhost:${PORT}`)
+})
